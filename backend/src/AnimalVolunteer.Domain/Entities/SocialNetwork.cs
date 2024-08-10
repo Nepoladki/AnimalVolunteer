c986123fb6bd7438f5772401be84d26a1280b809
Ilya <79629242222@ya.ru>
2024-08-10 13:38:43 +0300

    Volunteer

---
+++ backend/src/AnimalVolunteer.Domain/Entities/SocialNetwork.cs
@@ -0,0 +1,7 @@
+﻿namespace AnimalVolunteer.Domain.Entities;
+
+public class SocialNetwork
+{
+    public string Name { get; private set; } = null!;
+    public string Link { get; private set; } = null!;
+}
