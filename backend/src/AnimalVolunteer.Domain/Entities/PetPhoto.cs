56e5b4b3e75d5584ee58e5e6541724f1c794ce86
Ilya <79629242222@ya.ru>
2024-08-12 22:11:48 +0300

    Added PetPhoto

---
+++ backend/src/AnimalVolunteer.Domain/Entities/PetPhoto.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimalVolunteer.Domain.Entities;
+
+public class PetPhoto
+{
+    public Guid Id { get; private set; }
+    public string FilePath { get; private set; } = null!;
+    public bool IsMain { get; private set; }
+}
