3f59726080e431963c823b0561b1d5beb1aef79e
Ilya <79629242222@ya.ru>
2024-08-13 23:27:28 +0300

    Started 4.1

---
+++ backend/src/AnimalVolunteer.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AnimalVolunteer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AnimalVolunteer.Infrastructure;
+
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
+{
+    private const string CONNECTION_NAME = "Postgres";
+    public DbSet<Volunteer> Volunteers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString(CONNECTION_NAME));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+    }
+
+    private ILoggerFactory CreateLoggerFactory() =>
+        LoggerFactory.Create(builder => builder.AddConsole());
+}
