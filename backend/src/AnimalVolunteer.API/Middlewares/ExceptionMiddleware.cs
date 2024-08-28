3ab25c15ed69d924421dcc120c9ba5ebfb42bb33
Nepoladki <user@WIN-JT0FB1770TD>
2024-08-28 12:23:08 +0300

    5.5

---
+++ backend/src/AnimalVolunteer.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using AnimalVolunteer.API.Response;
+using System.Net;
+
+namespace AnimalVolunteer.API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private ILogger _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            var responseError = new ResponseError("server.internal", ex.Message, null);
+            var envelope = Envelope.Error([responseError]);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
