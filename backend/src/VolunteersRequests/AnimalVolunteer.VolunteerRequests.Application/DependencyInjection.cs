9a2beb2b993c5b920d36d37d609a2166451a65f3
Nepoladki <79629242222@ya.ru>
2024-11-14 18:57:11 +0300

    Added Application layer DI

--- backend/src/VolunteersRequests/AnimalVolunteer.VolunteerRequests.Application/DependencyInjection.cs
+++ backend/src/VolunteersRequests/AnimalVolunteer.VolunteerRequests.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AnimalVolunteer.Core.Abstractions.CQRS;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Runtime.CompilerServices;
 
@@ -9,9 +10,26 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(
         this IServiceCollection services, IConfiguration config)
     {
-        
+        return services
+            .AddQueries()
+            .AddCommands();
+    }
 
-        return services;
+    private static IServiceCollection AddCommands(this IServiceCollection services)
+    {
+        return services.Scan(scan => scan.FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(c => c
+                .AssignableToAny(typeof(ICommandHandler<,>), typeof(ICommandHandler<>)))
+                .AsSelfWithInterfaces()
+                .WithScopedLifetime());
+    }
+    private static IServiceCollection AddQueries(this IServiceCollection services)
+    {
+        return services.Scan(scan => scan.FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(c => c
+                .AssignableTo(typeof(IQueryHandler<,>)))
+                .AsSelfWithInterfaces()
+                .WithScopedLifetime());
     }
 }
 
